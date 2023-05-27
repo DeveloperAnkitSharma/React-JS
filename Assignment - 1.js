24c2f589ce9068c44f4a3e8694b1732c8a58e15c
DeveloperAnkitSharma <134158898+DeveloperAnkitSharma@users.noreply.github.com>
2023-05-27 11:33:25 +0530

    Add files via upload

---
+++ Assignment - 1.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { ThemeContext } from './theme-context'
+
+function App() {
+  const { theme, toggle, dark } = React.useContext(ThemeContext)
+
+  return (
+    <div className="App">
+      <header
+        className="App-header"
+        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <button
+          type="button"
+          onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: 'none'
+          }}
+        >
+          Toggle to {!dark ? 'Dark' : 'Light'} theme
+        </button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
